bbdb567ccd87ab9226b5ce013bf45a5ccd2fbd98
lorenz <Lorenz.Hasler@student.uibk.ac.at>
2018-11-08 21:05:02 +0100

    google test

--- exercise04/n_queens_problem.cpp
+++ exercise04/n_queens_problem.cpp
@@ -1,103 +1,94 @@
 
 #include <iostream>
 #include <string.h>
-
-#define N 14
+#include <vector>
 
 using namespace std;
-  
-  int numberOfResults = 0;
-  
+
+int numberOfResults = 0;
+
 //print solution
-void printSolution(int board[N][N]) { 
-    static int k = 1; 
-    cout<<k++<< "-\n"; 
-    for (int i = 0; i < N; i++) { 
-        for (int j = 0; j < N; j++) 
-            cout<<" "<< board[i][j]<<" "; 
-        cout<<"\n"; 
-    } 
-    cout<<"\n"; 
+void printSolution(vector<vector<int>> board) { 
+	static int k = 1; 
+	cout<<k++<< "-\n"; 
+	for (int i = 0; i < board.size(); i++) { 
+		for (int j = 0; j < board.size(); j++) 
+			cout<<" "<< board[i][j]<<" "; 
+		cout<<"\n"; 
+	} 
+	cout<<"\n"; 
 } 
   
 /*
  * looks if a queen can be placed on this position (checks diagonals and 
  * column)
  */
-bool isSafe(int board[N][N], int row, int col) { 
+bool isSafe(vector<vector<int>> board, int row, int col) { 
   
-    /* Check this row on left side */
-    //#pragma omp parallel
-    for (int i = 0; i < col; i++) {
+	/* Check this row on left side */
+	//#pragma omp parallel
+	for (int i = 0; i < col; i++) {
 		//#pragma omp critical
-        if (board[row][i]) {
-            return false; 
+		if (board[row][i]) {
+			return false; 
 		}
 	}
 	
-    /* Check upper diagonal on left side */
-    //#pragma omp parallel
-    for (int i=row, j=col; i>=0 && j>=0; i--, j--) {
+	/* Check upper diagonal on left side */
+	//#pragma omp parallel
+	for (int i=row, j=col; i>=0 && j>=0; i--, j--) {
 		//#pragma omp critical
-        if (board[i][j]) {
-            return false; 
+		if (board[i][j]) {
+			return false; 
 		}
 	}
   
-    /* Check lower diagonal on left side */
-    //#pragma omp parallel
-    for (int i=row, j=col; j>=0 && i<N; i++, j--){ 
+	/* Check lower diagonal on left side */
+	//#pragma omp parallel
+	for (int i=row, j=col; j>=0 && i<board.size(); i++, j--){ 
 		//#pragma omp critical
-        if (board[i][j]) {
-            return false; 
+		if (board[i][j]) {
+			return false; 
 		}
 	}
-    return true; 
+	return true; 
 } 
   
 /* A recursive utility function to solve N 
 Queen problem */
-bool solveNQUtil(int board[N][N], int col) { 
-    
-    /* all queens are placed*/
-    if (col == N) { 
-        //printSolution(board); 
-        numberOfResults++; 
-        return true;
-    } 
+bool solveNQUtil(vector<vector<int>> board, int col) { 
+	
+	/* all queens are placed*/
+	if (col == board.size()) { 
+		//printSolution(board); 
+		numberOfResults++; 
+		return true;
+	} 
   
-    /* Consider column and try placing this queen in all rows*/
-    bool res = false; 
-    for (int i = 0; i < N; i++) { 
-        if ( isSafe(board, i, col) ) { 
-            board[i][col] = 1; 
-            if(solveNQUtil(board, col + 1) || res){
+	/* Consider column and try placing this queen in all rows*/
+	bool res = false; 
+	for (int i = 0; i < board.size(); i++) { 
+		if ( isSafe(board, i, col) ) { 
+			board[i][col] = 1; 
+			if(solveNQUtil(board, col + 1) || res){
 				res = true;
 			} 
-            board[i][col] = 0; // BACKTRACK 
-        } 
-    } 
-    return res; 
+			board[i][col] = 0; // BACKTRACK 
+		} 
+	} 
+	return res; 
 } 
   
 /* This function solves the N Queen problem using 
 Backtracking. */
-void solveNQ() { 
-    int board[N][N];
-    //set Matrix to 0
-    memset(board, 0, sizeof(board)); 
+int solveNQ(int N) { 
+	vector<vector<int>> board(N, vector<int>(N));;
+	
+	if (solveNQUtil(board, 0) == false) { 
+		printf("Solution does not exist"); 
+		return 0; 
+	} 
   
-    if (solveNQUtil(board, 0) == false) { 
-        printf("Solution does not exist"); 
-        return ; 
-    } 
-  
-    return ; 
+	return numberOfResults; 
 } 
-  
-// driver program to test above function 
-int main() { 
-    solveNQ(); 
-    cout<<"N = "<<N<<"\nNumber of Results: "<<numberOfResults<<"\n";
-    return 0; 
-} 
+ 
