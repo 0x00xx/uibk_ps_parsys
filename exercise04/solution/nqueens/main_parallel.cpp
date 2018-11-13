509c16c354f70cba8b69891ecdf87fed19b6f3f9
lorenz <Lorenz.Hasler@student.uibk.ac.at>
2018-11-13 20:23:53 +0100

    bench nqueens

--- exercise04/solution/nqueens/main_parallel.cpp
+++ exercise04/solution/nqueens/main_parallel.cpp
@@ -1,8 +1,9 @@
 #include "n_queens_problem_parallel.cpp"
 #include <iostream>
 
-int main(){
-	
-	std::cout << solve_parallel(15) << "\n";
+int main(int argc, char** argv){
+	if (argc < 2) return EXIT_FAILURE;
+    int arraySize = atoi(argv[1]);
+	std::cout << solve_parallel(arraySize) << "\n";
 	return 0;
 }
--- exercise04/solution/nqueens/main_seq.cpp
+++ exercise04/solution/nqueens/main_seq.cpp
@@ -1,8 +1,10 @@
 #include "n_queens_problem_seq.cpp"
 #include <iostream>
 
-int main(int argc, char* argv[]){
-	
-	std::cout << solve(15) << "\n";
+int main(int argc, char** argv){
+	if (argc < 2) return EXIT_FAILURE;
+    int arraySize = atoi(argv[1]);
+    
+	std::cout << solve(arraySize) << "\n";
 	return 0;
 }
