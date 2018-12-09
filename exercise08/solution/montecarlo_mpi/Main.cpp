b40b2dc9639a22884c802e5f31fbc6f0c6f5e1ef
lorenz <Lorenz.Hasler@student.uibk.ac.at>
2018-12-09 23:20:24 +0100

    task 1 done

---
+++ exercise08/solution/montecarlo_mpi/Main.cpp
@@ -0,0 +1,44 @@
+// g++ Main.cpp Montecarlo.h Montecarlo.cpp -fopenmp -Wall -O3 -std=c++11 -o montecarlo"
+
+#include "Montecarlo.h"
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+
+  long s = 10000;
+  char *temp;
+  if (argc >= 2) {
+    s = strtoul(argv[1],&temp,0);
+  }
+  
+  int rank, size = 0;
+  double result = 0.0;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(size >= 2){
+	  size--;
+  }
+  
+  if(rank == 0 && size != 0){
+	  double *value = new double[size];
+	  
+	  MPI_Recv(value, size, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	  for(int i = 0; i<size; i++){
+		  result += value[i];
+	  }
+	  std::cout << result << std::endl;
+	  free(value);
+  }else if (argc == 3) {
+	  double value = montecarloPar(s/size, rank);
+	  MPI_Send(&value, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+	  //gmp_printf("Verification: %.20Ff\n", result);
+  }else{
+	  double value = montecarloSeq(s/size, rank);
+	  MPI_Send(&value, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+	  //gmp_printf("Verification: %.20Ff\n", result);
+  }
+  
+  MPI_Finalize();
+
+}
---
+++ exercise08/solution/montecarlo_mpi/Montecarlo.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <omp.h>
+#include <random>
+
+
+#ifndef MONTECARLO_H
+#define MONTECARLO_H
+
+double get_random();
+double montecarloSeq(long s, int rank);
+double montecarloPar(long s, int rank);
+
+#endif //MONTECARLO_H
