7f3e8c663044eebd75181bf02a88d63074e14762
lorenz <Lorenz.Hasler@student.uibk.ac.at>
2019-01-07 20:13:50 +0100

    bug fixing on cluster

--- exercise09/solution/Stencil.cpp
+++ exercise09/solution/Stencil.cpp
@@ -214,12 +214,12 @@ std::vector<double> * jacobi2DPar(const vector<double> &bounds, const double eps
 					(*blockIn)[blockSize*i+1] = left->at(blockSize*i+(blockSize-2));   
 				}
 			}else {	//middle
-				std::copy(bot->begin()+blockSize+1, bot->begin()+2*blockSize-1, blockIn->begin()+blockSize*(blockSize-2)+1);
+				/*std::copy(bot->begin()+blockSize+1, bot->begin()+2*blockSize-1, blockIn->begin()+blockSize*(blockSize-2)+1);
 				std::copy(top->begin()+blockSize*(blockSize-2)+1, top->begin()+blockSize*(blockSize-1)-1, blockIn->begin()+blockSize+1);
 				for(int i = 0; i<blockSize; i++){
 					(*blockIn)[blockSize*i+(blockSize-1)] = right->at(blockSize*i+1);  
 					(*blockIn)[blockSize*i+1] = left->at(blockSize*i+(blockSize-2)); 
-				}
+				}*/
 			}
 				
 			MPI_Barrier(MPI_COMM_WORLD);
